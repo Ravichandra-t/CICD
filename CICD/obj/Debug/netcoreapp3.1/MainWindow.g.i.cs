8a8f8e83c12954f6a139f20f5315c316af5edcc0
Ravichandra T <ravichandra.t@mistralsolutions.com>
2021-10-20 16:18:04 +0530

    next commit

--- CICD/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ CICD/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace CICD {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CICD;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CICD;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
